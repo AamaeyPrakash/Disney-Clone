9f7e829f5b53ececd458a8d457012b499fc43230
AamaeyPrakash <aamaeyprakash@gmail.com>
2024-07-22 18:03:56 +0530

    popup onclick

--- src/Components/HorizontalMovieCard.jsx
+++ src/Components/HorizontalMovieCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { HiMiniXMark } from "react-icons/hi2";
 
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
@@ -16,19 +17,22 @@ function HorizontalMovieCard({ movie }) {
     return (
         <>
             {popup && (
-                <div className='w-[1920px] h-[1920px] top-0 fixed'>
-                    <div className='w-[1920px] h-[1920px] fixed bg-[#313131] bg-opacity-75'></div>
-                    
-                    <div className='absolute bg-[#f1f1f1] px-[14px] py-[28px] rounded max-w-[600px] min-w-[300px] text-black'>
-                        <h2>{movie.title}</h2>
-                        <p>{movie.overview}</p>
-                        <img src={IMAGE_BASE_URL + movie.backdrop_path}></img>
-                        <button className='absolute px-[5px] py-[7px] bg-white' onClick={togglePopup}>CLOSE</button>
+                <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75'>
+                    <div className='relative bg-gray-900 text-white p-6 rounded-lg max-w-xl w-full'>
+                        <button className='absolute top-4 right-4 text-white bg-gray-800 rounded-full p-2'onClick={togglePopup}><HiMiniXMark className=''/></button>
+                        <img src={IMAGE_BASE_URL + movie.backdrop_path} alt={movie.title} className='w-full rounded-md mb-4'/>
+                        <div className='flex '>
+                            <p className='pr-2'>{movie.vote_average}</p>
+                            <p className='px-2'>{movie.release_date}</p>
+                        </div>
+                        <h2 className='text-2xl font-semibold mb-2'>{movie.title}</h2>
+                        <p className='text-lg'>{movie.overview}</p>
+
                     </div>
                 </div>
             )}
             <div className='hover:scale-110 transition-all duration-150 ease-in' onClick={handleClick}>
-                <img src={IMAGE_BASE_URL + movie.backdrop_path} className='w-[110px] md:w-[260px] rounded-lg hover:border-[3px] border-gray-400 cursor-pointer' />
+                <img src={IMAGE_BASE_URL + movie.backdrop_path} className='w-[110px] md:w-[260px] rounded-lg hover:border-2 border-gray-400 cursor-pointer' alt={movie.title}/>
                 <h2 className='w-[110px] md:w-[260px] text-white mt-2'>{movie.title}</h2>
             </div>
         </>
